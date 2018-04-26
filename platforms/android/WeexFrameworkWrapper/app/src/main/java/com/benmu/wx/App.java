0867a088f15a1d24ee76d1e3786a6ead99ebde71
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-04-26 20:05:06 +0800

    commit template

--- platforms/android/WeexFrameworkWrapper/app/src/main/java/com/benmu/wx/App.java
+++ platforms/android/WeexFrameworkWrapper/app/src/main/java/com/benmu/wx/App.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.benmu.framework.BMInitConfig;
 import com.benmu.framework.BMWXApplication;
 import com.benmu.framework.BMWXEngine;
+import com.benmu.framework.constant.Constant;
 
 /**
  * Created by Carry on 2017/8/23.
@@ -17,10 +18,7 @@ public class App extends BMWXApplication {
     public void onCreate() {
         super.onCreate();
         mInstance = this;
-
-
-
-
+        Constant.setApplicationId(BuildConfig.PAGE_PREFIX);
     }
 
 }
--- platforms/android/WeexFrameworkWrapper/app/src/main/java/com/benmu/wx/activity/SplashActivity.java
+++ platforms/android/WeexFrameworkWrapper/app/src/main/java/com/benmu/wx/activity/SplashActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.alibaba.fastjson.JSON;
+import com.benmu.erosplugingt.manager.GetuiManager;
+import com.benmu.erospluginumeng.event.EventUmeng;
+import com.benmu.erospluginumeng.model.UmengPlagformBean;
 import com.benmu.framework.BMWXEnvironment;
 import com.benmu.framework.constant.Constant;
 import com.benmu.framework.constant.WXConstant;
@@ -16,6 +20,7 @@ import com.benmu.framework.model.RouterModel;
 import com.benmu.framework.model.TitleModel;
 import com.benmu.framework.model.WeexEventBean;
 import com.benmu.wx.R;
+import com.plugamap.manager.GeoManager;
 
 /**
  * Created by Carry on 2017/8/23.
@@ -29,6 +34,7 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         init();
+        initPlugin();
     }
 
     private void init() {
@@ -48,6 +54,18 @@ public class SplashActivity extends Activity {
         }).start();
 
 
+    }
+
+    private void initPlugin() {
+        GetuiManager.pushInit(getApplication());
+        GeoManager mGeoManager = ManagerFactory.getManagerService(GeoManager.class);
+        mGeoManager.initAmap("a3308e6aef150346915922d2ea292590");
+        EventUmeng umeng = new EventUmeng();
+        umeng.initUM(getApplication(), "5a0194ad8f4a9d7f85000108");
+        UmengPlagformBean bean = new UmengPlagformBean();
+        bean.setAppKey("wxc47b78d8ef92e00c");
+        bean.setAppSecret("641ac311fd075c1b61fd8dbddc3d8168");
+        umeng.initPlatform(getApplication(), JSON.toJSONString(bean));
     }
 
     private void toHome() {
