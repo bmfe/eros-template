dac4062e7b4c45ec2ba3dcd6f1061f626dd29c55
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-06-20 17:57:57 +0800

    commit delete plugin library

--- platforms/android/WeexFrameworkWrapper/install.sh
+++ platforms/android/WeexFrameworkWrapper/install.sh
@@ -3,22 +3,10 @@ echo "开始更新依赖库"
 rm -r wxframework/
 rm -r sdk/
 rm -r nexus/
-rm -r ErosPluginAmap/
-rm -r erosplugingt/
-rm -r erospluginwxpay/
-rm -r erospluginumeng/
 
-git clone https://github.com/bmfe/WeexErosFramework.git "wxframework" 
+git clone https://github.com/bmfe/WeexErosFramework.git "wxframework"
 
-git clone https://github.com/bmfe/eros-nexus.git "nexus" 
-
-git clone https://github.com/bmfe/eros-plugin-android-amap.git "ErosPluginAmap" 
-
-git clone https://github.com/bmfe/eros-plugin-android-getui.git "erosplugingt" 
-
-git clone https://github.com/bmfe/eros-plugin-android-wxpay.git "erospluginwxpay" 
-
-git clone https://github.com/bmfe/eros-plugin-android-wxshare.git "erospluginumeng" 
+git clone https://github.com/bmfe/eros-nexus.git "nexus"
 cd ../
 
 echo "依赖库更新完成"
