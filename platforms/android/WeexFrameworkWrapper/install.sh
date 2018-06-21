2c4791b4a268ce9dfbd31a2b6a9cddc0a41ad0b4
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-06-21 10:24:33 +0800

    commit install.sh

--- platforms/android/WeexFrameworkWrapper/install.sh
+++ platforms/android/WeexFrameworkWrapper/install.sh
@@ -8,17 +8,17 @@ rm -r erosplugingt/
 rm -r erospluginwxpay/
 rm -r erospluginumeng/
 
-git clone https://github.com/bmfe/WeexErosFramework.git "wxframework" 
+git clone https://github.com/bmfe/WeexErosFramework.git "wxframework"
 
-git clone https://github.com/bmfe/eros-nexus.git "nexus" 
+git clone https://github.com/bmfe/eros-nexus.git "nexus"
 
-git clone https://github.com/bmfe/eros-plugin-android-amap.git "ErosPluginAmap" 
+git clone https://github.com/bmfe/eros-plugin-android-amap.git "ErosPluginAmap"
 
-git clone https://github.com/bmfe/eros-plugin-android-getui.git "erosplugingt" 
+git clone https://github.com/bmfe/eros-plugin-android-getui.git "erosplugingt"
 
-git clone https://github.com/bmfe/eros-plugin-android-wxpay.git "erospluginwxpay" 
+git clone https://github.com/bmfe/eros-plugin-android-wxpay.git "erospluginwxpay"
 
-git clone https://github.com/bmfe/eros-plugin-android-wxshare.git "erospluginwxshare" 
+git clone https://github.com/bmfe/eros-plugin-android-wxshare.git "erospluginumeng"
 
 cd ../
 
