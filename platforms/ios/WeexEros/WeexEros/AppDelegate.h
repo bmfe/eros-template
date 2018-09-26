57ca098900b040bcb9710ccd3fdf19977cdf060b
xionghuayu <xionghuayu@benmu-health.com>
2018-09-26 18:41:13 +0800

    update demo

--- platforms/ios/WeexEros/WeexEros/AppDelegate.h
+++ platforms/ios/WeexEros/WeexEros/AppDelegate.h
@@ -3,11 +3,11 @@
 //  WeexEros
 //
 //  Created by XHY on 2017/8/7.
-//  Copyright © 2017年 benmu. All rights reserved.
+//  Copyright © 2017年 Byte Master. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import <BMBaseLibrary/BMAppDelegate.h>
+#import <ErosPluginBaseLibrary/BMAppDelegate.h>
 
 @interface AppDelegate : BMAppDelegate
 
